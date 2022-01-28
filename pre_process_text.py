b99c4825127b60484c28171656edc99ce656b3fe
Marlies Monch <marlies.monch@gmail.com>
2022-01-28 10:52:09 +0100

    latest version of the pre_process_text.py file

--- pre_process_text.py
+++ pre_process_text.py
@@ -6,18 +6,21 @@ def remove_accented_chars(text):
   return text
 
 # preprocessing
-def pre_process_corpus(docs):
+def pre_process_text(labels):
   norm_docs = []
-  for string in tqdm.tqdm(docs):
+  for string in tqdm.tqdm(labels):
     string = string.replace("_", " ")
     string = string.translate(string.maketrans("\n\t\r", "   "))
     string = remove_accented_chars(string)
     # insert space where an uppercase letter follows a lowercase letter
-    string = re.sub(r'(?<![A-Z\W])(?=[A-Z])', ' ', string)
+    string = re.sub(r"(?<![A-Z\W])(?=[A-Z])", " ", string)
     string = contractions.fix(string)
+    # where XXXX number to XXXX number replace - with to IN PROGRESS
+    string = string.replace("-", " to ")
     # remove special characters or whitespaces
     string = re.sub(r"[^a-zA-Z0-9\s]", "", string, flags=re.I|re.A)
     string = string.lower()
     string = string.strip()
+    string = string.split(" ")
     norm_docs.append(string)
   return norm_docs
